297d6ed026a20a2f6cab2407fd5d5db4957c2b56
SharwanKunwar <sharwankunwar07@gmail.com>
2026-07-22 12:16:48 +0545

    practiced notification with losely coppled way

--- Take04/DemoNotificationSystem/src/main/java/com/unpredictableXpractice/DemoNotificationSystem/DemoNotificationSystemApplication.java
+++ Take04/DemoNotificationSystem/src/main/java/com/unpredictableXpractice/DemoNotificationSystem/DemoNotificationSystemApplication.java
@@ -2,6 +2,7 @@ package com.unpredictableXpractice.DemoNotificationSystem;
 
 import com.unpredictableXpractice.DemoNotificationSystem.notification.EmailService;
 import com.unpredictableXpractice.DemoNotificationSystem.notification.NotificationServiceHelper;
+import com.unpredictableXpractice.DemoNotificationSystem.notification.PopUpNotificationService;
 import com.unpredictableXpractice.DemoNotificationSystem.notification.SmsService;
 import com.unpredictableXpractice.DemoNotificationSystem.service.OrderService;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +14,9 @@ import javax.management.Notification;
 @SpringBootApplication
 public class DemoNotificationSystemApplication {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(DemoNotificationSystemApplication.class, args);
-		NotificationServiceHelper n = new SmsService();
+		NotificationServiceHelper n = new PopUpNotificationService();
 
 		OrderService orderService = new OrderService();
 		orderService.placeOrder(n);
--- Take04/DemoNotificationSystem/src/main/java/com/unpredictableXpractice/DemoNotificationSystem/service/OrderService.java
+++ Take04/DemoNotificationSystem/src/main/java/com/unpredictableXpractice/DemoNotificationSystem/service/OrderService.java
@@ -1,12 +1,17 @@
 package com.unpredictableXpractice.DemoNotificationSystem.service;
-
-import com.unpredictableXpractice.DemoNotificationSystem.notification.EmailService;
 import com.unpredictableXpractice.DemoNotificationSystem.notification.NotificationServiceHelper;
 
 public class OrderService
 {
-    public void placeOrder(NotificationServiceHelper n) {
-        System.out.println("Placing order...");
+    public void placeOrder(NotificationServiceHelper n) throws InterruptedException {
+        System.out.print("Placing order.");
+        int i=0;
+        while(i!=3){
+            Thread.sleep(1000);
+            System.out.print(".");
+            i++;
+        }
+        System.out.println("\n\nMessage: ");
         n.sendNotification();
 
     }
