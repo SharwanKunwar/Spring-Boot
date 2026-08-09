e4ec25c4d8436f09af06a60f3eff77dcc45cca56
SharwanKunwar <sharwankunwar07@gmail.com>
2026-08-09 17:00:09 +0545

    updated main for initilize delivery : so lazy initilization is the processes of initilization when needed

--- Spring Core/BeanInitilization/src/main/java/org/unpredictableXpractice/Main.java
+++ Spring Core/BeanInitilization/src/main/java/org/unpredictableXpractice/Main.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.unpredictableXpractice.config.AppConfig;
+import org.unpredictableXpractice.service.DeliveryService;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,5 +12,8 @@ public class Main {
     static void main()
     {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        DeliveryService deliveryService = (DeliveryService) context.getBean("deliveryService");
+
     }
 }
