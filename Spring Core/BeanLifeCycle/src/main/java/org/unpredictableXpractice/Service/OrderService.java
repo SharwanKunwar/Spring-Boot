5b3a8803afb6713da1fa739ce41665f071319743
SharwanKunwar <sharwankunwar07@gmail.com>
2026-08-10 11:21:46 +0545

    added beanlifecycle demo project

---
+++ Spring Core/BeanLifeCycle/src/main/java/org/unpredictableXpractice/Service/OrderService.java
@@ -0,0 +1,9 @@
+package org.unpredictableXpractice.Service;
+
+public class OrderService
+{
+    public void placeOrder()
+    {
+        System.out.println("Order Placed");
+    }
+}
---
+++ Spring Core/BeanLifeCycle/src/main/java/org/unpredictableXpractice/Service/PaymentService.java
@@ -0,0 +1,8 @@
+package org.unpredictableXpractice.Service;
+
+public class PaymentService
+{
+    public void pay(){
+        System.out.println("Payment done");
+    }
+}
