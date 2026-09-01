481b75fc33a3f5863c868088a09982eaf7f7cffc
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-01 18:16:17 +0545

    updated cotroller

--- Spring Boot/CrudPractice/MovieHub/src/main/java/com/unpredictableXMovies/MovieHub/controller/MovieController.java
+++ Spring Boot/CrudPractice/MovieHub/src/main/java/com/unpredictableXMovies/MovieHub/controller/MovieController.java
@@ -6,10 +6,9 @@ import com.unpredictableXMovies.MovieHub.service.MovieServiceHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -22,6 +21,13 @@ public class MovieController
     @PostMapping("/create")
     public ResponseEntity<MovieResponseDTO> create(@RequestBody MovieRequestDTO requestDTO){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.createMovie(requestDTO));
+    }
+
+    // Get all movies
+    @GetMapping("/all")
+    public ResponseEntity<List<MovieResponseDTO>> getAllMovies()
+    {
+        return ResponseEntity.ok(service.getAllMovies());
     }
 
 
