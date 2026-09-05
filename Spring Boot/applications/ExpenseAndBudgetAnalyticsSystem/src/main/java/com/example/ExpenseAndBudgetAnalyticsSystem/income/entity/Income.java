47beb7f32a98cd93d04371dc8a629a86bac68a5a
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-05 15:43:07 +0545

    updated income

---
+++ Spring Boot/applications/ExpenseAndBudgetAnalyticsSystem/src/main/java/com/example/ExpenseAndBudgetAnalyticsSystem/income/entity/Income.java
@@ -0,0 +1,57 @@
+package com.example.ExpenseAndBudgetAnalyticsSystem.income.entity;
+
+import com.example.ExpenseAndBudgetAnalyticsSystem.income.enums.Source;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "income")
+public class Income
+{
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Source source;
+
+    @Column(nullable = false)
+    private LocalDateTime incomeDate;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
---
+++ Spring Boot/applications/ExpenseAndBudgetAnalyticsSystem/src/main/java/com/example/ExpenseAndBudgetAnalyticsSystem/income/enums/Source.java
@@ -0,0 +1,11 @@
+package com.example.ExpenseAndBudgetAnalyticsSystem.income.enums;
+
+public enum Source
+{
+    SALARY,
+    FREELANCE,
+    BUSINESS,
+    INVESTMENT,
+    GIFT,
+    OTHER
+}
