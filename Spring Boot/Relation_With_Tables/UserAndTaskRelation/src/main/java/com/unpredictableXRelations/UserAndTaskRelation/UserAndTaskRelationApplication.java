7e656b8bdfbd39d9d684a73ea27ab104e3319bfd
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-15 14:54:18 +0545

    added another project in relations and base system is ready to implement

---
+++ Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/UserAndTaskRelationApplication.java
@@ -0,0 +1,21 @@
+package com.unpredictableXRelations.UserAndTaskRelation;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAndTaskRelationApplication
+{
+	public static void main(String[] args)
+	{
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		SpringApplication.run(UserAndTaskRelationApplication.class, args);
+		System.out.println("Basic System Is Ready To Implement Business Logic");
+	}
+
+}
