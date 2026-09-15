d7183172744c0b26052888c965a63132f2fcb51a
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-15 15:42:35 +0545

    added exception where spring boot handle exceptions globally which incldue custom exception like : resource not found etc

---
+++ Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.unpredictableXRelations.UserAndTaskRelation.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler
+{
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException exception)
+    {
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+}
--- Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/user/controller/UserController.java
+++ Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/user/controller/UserController.java
@@ -3,12 +3,11 @@ package com.unpredictableXRelations.UserAndTaskRelation.user.controller;
 import com.unpredictableXRelations.UserAndTaskRelation.user.dtos.UserRequestDTO;
 import com.unpredictableXRelations.UserAndTaskRelation.user.dtos.UserResponseDTO;
 import com.unpredictableXRelations.UserAndTaskRelation.user.service.UserServiceHandler;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,12 +19,14 @@ public class UserController
     private final UserServiceHandler service;
 
     // create end point
-    public ResponseEntity<UserResponseDTO> create(@RequestBody UserRequestDTO requestDTO)
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> create(@Valid @RequestBody UserRequestDTO requestDTO)
     {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(requestDTO));
     }
 
     // Get all
+    @GetMapping
     public ResponseEntity<List<UserResponseDTO>> getAll()
     {
         return ResponseEntity.ok(service.getAllUser());
