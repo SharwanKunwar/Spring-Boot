3196f8094c23657ddd360542ab23e562e971995d
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-17 15:54:50 +0545

    added filter -> response header filter

---
+++ Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/filter/AuthFilter.java
@@ -0,0 +1,4 @@
+package com.unpredictableXRelations.UserAndTaskRelation.filter;
+
+public class AuthFilter {
+}
---
+++ Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/filter/ResponseHeaderFilter.java
@@ -0,0 +1,23 @@
+package com.unpredictableXRelations.UserAndTaskRelation.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ResponseHeaderFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        String requestId = UUID.randomUUID().toString();
+
+        httpServletResponse.setHeader("x-request-id", requestId);
+        chain.doFilter(request, response);
+
+    }
+}
