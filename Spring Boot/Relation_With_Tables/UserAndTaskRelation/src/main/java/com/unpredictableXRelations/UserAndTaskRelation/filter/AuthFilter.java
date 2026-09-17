e2803de181cc384e591d8b290f19574bcd695a19
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-17 16:01:15 +0545

    updated auth filter with token checking

--- Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/filter/AuthFilter.java
+++ Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/filter/AuthFilter.java
@@ -1,4 +1,26 @@
 package com.unpredictableXRelations.UserAndTaskRelation.filter;
 
-public class AuthFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        String token = httpServletRequest.getHeader("token");
+        if(token == null || !token.equals("123"))
+        {
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        chain.doFilter(request, response);
+
+    }
 }
