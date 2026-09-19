fdc09c28a8f9b9dd52ef10edad50e13e503c263f
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-19 13:12:03 +0545

    added new concept practice project which is interceptors

---
+++ Spring Boot/Interceptors/InterceptorFilter/src/test/java/com/example/InterceptorFilter/InterceptorFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InterceptorFilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
