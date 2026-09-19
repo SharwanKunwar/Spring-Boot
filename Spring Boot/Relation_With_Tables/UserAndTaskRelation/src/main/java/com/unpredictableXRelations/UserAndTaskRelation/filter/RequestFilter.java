457610f39b61f05d59b935316c17bcf0fa7f8ed0
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-19 10:23:07 +0545

    we printed the request body

--- Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/filter/RequestFilter.java
+++ Spring Boot/Relation_With_Tables/UserAndTaskRelation/src/main/java/com/unpredictableXRelations/UserAndTaskRelation/filter/RequestFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @Component
@@ -22,5 +23,20 @@ public class RequestFilter implements Filter
         // setting header
         // we don't wanna modify url header
         // other things as well but there is a exception if we want we can but it's gets cold anyway.
+
+        BufferedReader reader = httpServletRequest.getReader();
+
+        StringBuilder body = new StringBuilder();
+        String line = reader.readLine();
+
+        while(line != null)
+        {
+            body.append(line);
+            line = reader.readLine();
+        }
+        System.out.println(body);
+
+        chain.doFilter(request, response);
+
     }
 }
