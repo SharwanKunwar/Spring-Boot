419f301e3e5e4ccbae02bb9d49997f80e6639cb5
SharwanKunwar <sharwankunwar07@gmail.com>
2026-09-19 17:01:31 +0545

    using method handler to get controller name and method name for now

--- Spring Boot/Interceptors/InterceptorFilter/src/main/java/com/example/InterceptorFilter/interceptor/LoginInterceptor.java
+++ Spring Boot/Interceptors/InterceptorFilter/src/main/java/com/example/InterceptorFilter/interceptor/LoginInterceptor.java
@@ -1,10 +1,10 @@
 package com.example.InterceptorFilter.interceptor;
 
 
-import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,19 +14,26 @@ public class LoginInterceptor implements HandlerInterceptor
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
     {
-        System.out.println("pre handler called");
+        System.out.println("--------------------------------------------- Pre Handler scope -----");
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        String controllerName = handlerMethod.getBeanType().getName();
+        String methodName = handlerMethod.getMethod().getName();
+
+        System.out.println("Controller : "+controllerName);
+        System.out.println("Method : "+methodName);
+        System.out.println("--------------------------------------------------------\n");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
     {
-        System.out.println("post handle called.");
+        System.out.println("--------------------------------------------- Post Handler scope -----");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception
     {
-        System.out.println("after completion called.");
+        System.out.println("--------------------------------------------- After Completion Handler scope -----");
     }
 }
